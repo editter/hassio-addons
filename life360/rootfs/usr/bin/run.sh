86bad094a8d6aa4afcd70a1a809564ac1f0df0d6
Ditter <editter@skylinetechnologies.com>
2018-08-23 20:18:42 -0500

    Fixed Security

--- life360/rootfs/usr/bin/run.sh
+++ life360/rootfs/usr/bin/run.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/with-contenv bash
-# ==============================================================================
-#
-# Community Hass.io Add-ons: Example
-#
-# Example add-on for Hass.io.
-# This add-on displays a random quote every X seconds.
-#
-# ==============================================================================
 set -o errexit  # Exit script when a command exits with non-zero status
 set -o errtrace # Exit on error inside any functions or sub-shells
 set -o nounset  # Exit script on use of an undefined variable
 set -o pipefail # Return exit status of the last command in the pipe that failed
 
 # shellcheck disable=SC1091
-source /usr/lib/hassio-addons/base.sh
+# source /usr/lib/hassio-addons/base.sh
 
 # npm run start
 node server.js
